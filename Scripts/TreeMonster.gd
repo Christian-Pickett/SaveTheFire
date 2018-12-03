aea89e5d735e63b7bc61a2e0b49cbc7351c08385
Christian Pickett <christianpickett@protonmail.com>
2018-12-02 21:27:04 -0800

    made updates to animations, cleaned directory, changed gameplay

--- Scripts/TreeMonster.gd
+++ Scripts/TreeMonster.gd
@@ -1,17 +1,19 @@
 extends KinematicBody2D
 
 var direction = Vector2()
-var HP = 60
+var HP = 12
 var onFire = false
+var death = false
 
 func _ready():
 	add_to_group("Tree_Monster")
 	$AnimatedSprite.play("noFire")
 
 func _process(delta):
-	direction.x = cos(self.get_angle_to(Global.Player.position))
-	direction.y = sin(self.get_angle_to(Global.Player.position))
-	move_and_slide(direction * 100)
+	if not death:
+		direction.x = cos(self.get_angle_to(Global.Player.position))
+		direction.y = sin(self.get_angle_to(Global.Player.position))
+		move_and_slide(direction * 100)
 	if onFire:
 		burn()
 
@@ -20,14 +22,20 @@ func scream():
 
 func burn():
 	onFire = true
-	$AnimatedSprite.play("onFire")
+	if not death:
+		$AnimatedSprite.play("onFire")
 	if HP != 0:
 		if $Timer.is_stopped():
 			HP = HP - 1
 			$Timer.start()
 	else:
-		queue_free()
-		get_tree().change_scene("res://Scenes/Victory.tscn")
+		if $Timer.is_stopped() and not death:
+			$AnimatedSprite.play("Death")
+			$Timer.start()
+			death = true
+		if death and $Timer.is_stopped():
+			queue_free()
+			get_tree().change_scene("res://Scenes/Victory.tscn")
 
 func douse():
 	onFire = false
